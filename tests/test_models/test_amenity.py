3596f64b2ac43a72e1d997bb50fe3abd9bc34953
Cassyeh <cassandraijezie@gmail.com>
2023-06-21 17:49:21 -0700

    Tests for amenity and city

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ """
 from tests.test_models.test_base_model import test_basemodel
+from models.base_model import BaseModel
 from models.amenity import Amenity
 
 
@@ -17,3 +18,11 @@ class test_Amenity(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+    def test_is_subclass(self):
+        """Test that Amenity is a subclass of BaseModel"""
+        amenity = Amenity()
+        self.assertIsInstance(amenity, BaseModel)
+        self.assertTrue(hasattr(amenity, "id"))
+        self.assertTrue(hasattr(amenity, "created_at"))
+        self.assertTrue(hasattr(amenity, "updated_at"))
--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ """
 from tests.test_models.test_base_model import test_basemodel
+from models.base_model import BaseModel
 from models.city import City
+from models import storage
 
 
 class test_City(test_basemodel):
@@ -22,3 +24,31 @@ class test_City(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+    def test_is_subclass(self):
+        """Test that City is a subclass of BaseModel"""
+        city = City()
+        self.assertIsInstance(city, BaseModel)
+        self.assertTrue(hasattr(city, "id"))
+        self.assertTrue(hasattr(city, "created_at"))
+        self.assertTrue(hasattr(city, "updated_at"))
+
+    def test_attr(self):
+        """Test that City has attribute name, and it's an empty string"""
+        new_city = City()
+        self.assertTrue(hasattr(new_city, "name"))
+        self.assertTrue(hasattr(new_city, "state_id"))
+        if storage == 'db':
+            self.assertEqual(new_city.name, None)
+            self.assertEqual(new_city.state_id, None)
+        else:
+            self.assertEqual(new_city.name, "")
+            self.assertEqual(new_city.state_id, "")
+        new_dict = new_city.to_dict()
+        self.assertEqual(type(new_dict), dict)
+        self.assertEqual(new_dict["__class__"], "City")
+        self.assertFalse("_sa_instance_state" in new_dict)
+        for attr in new_city.__dict__:
+            if attr != "_sa_instance_state":
+                self.assertTrue(attr in new_dict)
+        self.assertTrue("__class__" in new_dict)
