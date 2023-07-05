6287b328d99011450fd655be77960c1e5fbe1e06
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-07-05 21:42:26 +0100

    state, only orm

--- models/state.py
+++ models/state.py
@@ -3,25 +3,10 @@
 from .base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import os
-
-stored = os.environ.get('HBNB_TYPE_STORAGE')
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    if stored == 'db':
-        cities = relationship('City', cascade='all, delete-orphan')
-    else:
-        @property
-        def cities(self):
-            from models import storage
-            from .city import City
-            cities = []
-            all_cities = storage.all(City)
-            for key, city_obj in all_cities.items():
-                if city_obj.state_id == self.id:
-                    cities.append(city_obj)
-            return cities
+    cities = relationship('City', cascade='all, delete-orphan')
