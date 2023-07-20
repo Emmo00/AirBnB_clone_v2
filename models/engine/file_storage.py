74d6bb5a76bd4bb5774c83a6e90c57131140a338
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-07-20 12:02:26 -0700

    preamble

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -90,3 +90,7 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """close engine"""
+        self.reload()
