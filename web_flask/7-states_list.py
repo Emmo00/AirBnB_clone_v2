364da3e3c631b9c396d0b2100b17cfc4a1689b6b
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-07-21 10:00:00 -0700

    states list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""index only flask app"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """state list route"""
+    states = storage.all(State)
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000)
