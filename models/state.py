f635fe070d4512e00f2dfbfe053723553e840c9b
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-07-21 07:29:49 -0700

    pycode style

--- models/state.py
+++ models/state.py
@@ -11,7 +11,8 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', cascade='all, delete-orphan', backref='state')
+        cities = relationship('City', cascade='all, delete-orphan',
+                              backref='state')
     else:
         @property
         def cities(self):
