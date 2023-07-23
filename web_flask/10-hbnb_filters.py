79737b611c70f3bddb48322ac7d2e20482aeff7c
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-07-23 13:48:10 +0100

    10, states and amenities

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.amenity import Amenity
 
 app = Flask(__name__)
 
@@ -16,8 +17,19 @@ def close_db(exception=None):
 @app.route('/hbnb_filters', strict_slashes=False)
 def hbnb_filters():
     """hbnb filters route"""
-    return render_template("10-hbnb_filters.html")
+    states = list(storage.all(State).values())
+    states.sort(key=lambda x: x.name)
+
+    def get_cities(state):
+        cities = state.cities
+        cities.sort(key=lambda x: x.name)
+        return cities
+    amenities = list(storage.all(Amenity).values())
+    amenities.sort(key=lambda x: x.name)
+    return render_template("10-hbnb_filters.html",
+                           states=states, get_cities=get_cities,
+                           amenities=amenities)
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 5000)
+    app.run('0.0.0.0', 5000, debug=True)
